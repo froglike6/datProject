19925184251d9625abc6df868688bc4e2f1ef7de
froglike6 <froglike6@gmail.com>
2022-06-14 19:15:13 +0900

    Unused part delect

--- oil.c
+++ oil.c
@@ -14,11 +14,6 @@ int main(){
 	double gasoline[60], diesel[60], kerosene[60];
 	int car[60];
 	int temp=0, i;
-	double temp2=0, temp3=0;
-	
-	double meanGas, meanDie, meanKer, meanCar, meanYar;
-	double aGas, aDie, aKer, aCar;
-	double bGas, bDie, bKer, bCar;
 	
 	FILE *data = NULL;
 	data = fopen("data/oildata2.csv", "r");
@@ -34,53 +29,6 @@ int main(){
 		return -1;
 	}
 	fclose(data);
-	/*
-	Start point for linear regression
-	y=ax+b
-	*/
-	for (i=0; i<51; i++){
-		meanGas += gasoline[i];
-		meanDie += diesel[i];
-		meanKer += kerosene[i];
-		meanCar += car[i];
-		meanYar += year[i];
-	}
-	meanGas/=51;
-	meanDie/=51;
-	meanKer/=51;
-	meanCar/=51;
-	meanYar/=51;
-	//Mean value checked
-	
-	
-	for (i=0; i<51; i++){
-		temp2+=((gasoline[i]-meanGas)*(year[i]-meanYar));
-		temp3+=((year[i]-meanYar)*(year[i]-meanYar));
-	}
-	aGas = temp2/temp3;
-	bGas = meanGas-(aGas*meanYar);
-	temp2=0;
-	for (i=0; i<51; i++){
-		temp2+=((diesel[i]-meanDie)*(year[i]-meanYar));
-	}
-	aDie = temp2/temp3;
-	bDie = meanDie - aDie*meanYar;
-	temp2=0;
-	for (i=0; i<51; i++){
-		temp2+=(kerosene[i]-meanKer)*(year[i]-meanYar);
-	}
-	aKer = temp2/temp3;
-	bKer = meanKer - aKer*meanYar;
-	temp2=0;
-	for (i=0; i<51; i++){
-		temp2+=(car[i]-meanCar)*(year[i]-meanYar);
-	}
-	aCar = temp2/temp3;
-	bCar = meanCar - aCar*meanYar;
-	temp2=0, temp3=0;
-	
-	//printf("%lf, %lf, %lf, %lf", bGas, bDie, bKer, bCar);
-	
 	
 	/*
 	//Print data of file
@@ -121,7 +69,6 @@ void setup(){
 				}
 		}
 		else if (key == 13){
-			brk = 1;
 			switch (cur){
 				case 1:
 					dataShow();
@@ -134,12 +81,6 @@ void setup(){
 					exit(0);
 					break;
 			}
-		}
-		if (brk==1){
-			break;
-		}
-		else{
-			menu(cur);
 		}
 	}
 }
