8140cbfa158ed252a3c8adb481ba440816cd7820
froglike6 <froglike6@gmail.com>
2022-06-14 11:15:59 +0900

    Edited polynomial fitting part

--- oil.c
+++ oil.c
@@ -188,42 +188,24 @@ void dataShow(){
 	
 }
 
-void dataAnis(){
-	//no. of data-points
-    int N;  
-    //degree of polynomial
-    int n;  
-    printf("Enter the no. of data-points:\n");
-    scanf("%d",&N);
-    //arrays to store the c and y-axis data-points
-    double x[N], y[N];  
-    printf("Enter the x-axis values:\n");
+void dataAnis(){//please make case and menu
+    int N=51; //Number of data points
+    int n; //degree of polynomial
+    double y[N]; //array of x and y data points
     int i,j;
-    for(i=0;i<N;i++){
-        scanf("%lf",&x[i]);
-    }
-    printf("Enter the y-axis values:\n");
-    for(i=0;i<N;i++){
-        scanf("%lf",&y[i]);
-    }
-    printf("Enter the degree of polynomial to be used:\n");
-    scanf("%d",&n);
-    // an array of size 2*n+1 for storing N, Sig xi, Sig xi^2, ...., etc. which are the independent components of the normal matrix
     double X[2*n+1];  
     for(i=0;i<=2*n;i++){
         X[i]=0;
         for(j=0;j<N;j++){
-            X[i]=X[i]+pow(x[j],i);
+            X[i]=X[i]+pow(year[j],i);
         }
     }
-    //the normal augmented matrix
     double B[n+1][n+2];  
-    // rhs
     double Y[n+1];      
     for(i=0;i<=n;i++){
         Y[i]=0;
         for(j=0;j<N;j++){
-            Y[i]=Y[i]+pow(x[j],i)*y[j];
+            Y[i]=Y[i]+pow(year[j],i)*y[j];
         }
     }
     for(i=0;i<=n;i++){
@@ -235,8 +217,6 @@ void dataAnis(){
         B[i][n+1]=Y[i];
     }
     double A[n+1];
-    printf("The polynomial fit is given by the equation:\n");
-    printMatrix(n+1,n+2,B);
     gaussEliminationLS(n+1,n+2,B,A);
     for(i=0;i<=n;i++){
         printf("%lfx^%d+",A[i],i);
@@ -244,19 +224,7 @@ void dataAnis(){
      
 }
 
-/******************************************************
-*************Chi-square fitting**************
-Polynomial Fitting
-******************************************************/
-#include<stdio.h>
-#include<math.h>
-/*******
- Function that performs Gauss-Elimination and returns the Upper triangular matrix and solution of equations:
-There are two options to do this in C.
-1. Pass the augmented matrix (a) as the parameter, and calculate and store the upperTriangular(Gauss-Eliminated Matrix) in it.
-2. Use malloc and make the function of pointer type and return the pointer.
-This program uses the first option.
-********/
+
 void gaussEliminationLS(int m, int n, double a[m][n], double x[n-1]){
     int i,j,k;
     for(i=0;i<m-1;i++){
@@ -293,12 +261,3 @@ void gaussEliminationLS(int m, int n, double a[m][n], double x[n-1]){
              
 }
 
-void printMatrix(int m, int n, double matrix[m][n]){
-    int i,j;
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            printf("%lf\t",matrix[i][j]);
-        }
-        printf("\n");
-    } 
-}
